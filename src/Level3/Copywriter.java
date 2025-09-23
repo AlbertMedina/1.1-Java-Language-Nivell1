77cfd19070000d072dcf42e07ba7edf603fdf8a5
Albert Medina <albertmecu@gmail.com>
2025-09-23 22:13:13 +0200

    Added required classes and defined implemented pricing/rating logic

---
+++ src/Level3/Copywriter.java
@@ -0,0 +1,13 @@
+package Level3;
+
+public class Writer {
+
+    private String name;
+    private final String dni;
+    private static int salary = 1500;
+
+    public Writer(String name, String dni) {
+        this.name = name;
+        this.dni = dni;
+    }
+}
